92744cd0dd5747a19dc94d04dbec0f49dcdfbf2f
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-05-28 16:03:51 +0530

    [Anjaneya]refactor:flipcoin percentage calculation

---
+++ BasicCodeProgram/BasicCodeProgram/FlipCoinProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCodeProgram
+{
+    class FlipCoinProgram
+    {
+        public int heads = 0;
+        public int tails = 0;
+
+        public void FlipCoinPercentage()
+        {
+            Console.WriteLine("Enter how many times u want to flip");
+            int flipnumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int coinflip = 0; coinflip <= flipnumber; coinflip++)
+            {
+                Random random = new Random();
+                double flipcoin = random.NextDouble();
+                if (flipcoin < 0.5)
+                    tails++;
+                else
+                    heads++;
+            }
+            double percentagetails = (tails * 100) / flipnumber;
+            double percentageheads = (heads * 100) / flipnumber;
+            Console.WriteLine("percentage of heads" + percentageheads);
+            Console.WriteLine("percentage of tails" + percentagetails);
+        }
+
+    }
+}
---
+++ BasicCodeProgram/BasicCodeProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BasicCodeProgram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1:flip coin percentage");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    FlipCoinProgram flipcoin = new FlipCoinProgram();
+                    flipcoin.FlipCoinPercentage();
+                    break;
+
+            }
+           
+        }
+    }
+}
